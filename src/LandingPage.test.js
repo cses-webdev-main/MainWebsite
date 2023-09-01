fcf324c0788458daa8af313bad0cbe6274ff7a2e
Kevin Kim <kevink856@gmail.com>
2023-08-31 20:36:37 -0400

    Update LandingPage.test.js

--- src/LandingPage.test.js
+++ src/LandingPage.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import LandingPage from './components/LandingPage';
 
 test('renders learn react link', () => {
-  render(<LandingPage />);s
+  render(<LandingPage />);
   const linkElement = screen.getByText(/test/i);
   expect(linkElement).toBeInTheDocument();
 });
