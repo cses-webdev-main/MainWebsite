416c16988b16aa4e1fa828b6b119dee8e7dae526
Kevin Kim <kevink856@gmail.com>
2023-09-19 06:27:14 -0400

    Reformat Files + Initialize Tempaltes

---
+++ src/components/Media.js
@@ -0,0 +1,16 @@
+// Import Dependencies
+import '../styles/Media.css';
+import Navbar from './Navbar';
+
+function Media() {
+  return (
+    <div className="mediaContainer">
+      <header className="mediaNavbar">
+        <Navbar />
+
+      </header>
+    </div>
+  );
+}
+
+export default Media;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // Import Components
-import LandingPage from './components/LandingPage';
+import Home from './components/Home';
+import AboutUs from './components/AboutUs';
+import Events from './components/Events';
+import Media from './components/Media';
+
 
 // Create router to redirect by refreshing (HashRouting is not supported for GitHub Pages)
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,7 +18,19 @@ const router = createBrowserRouter([
   // Lists site URL endpoints and the respective pages to load
   {
       path: "/",
-      element: <LandingPage />,
+      element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <AboutUs />,
+  },
+  {
+    path: "/events",
+    element: <Events />,
+  },
+  {
+    path: "/media",
+    element: <Media />,
   },],
   {
       basename: "/MainWebsite",
