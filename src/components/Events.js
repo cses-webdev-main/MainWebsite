876ee63d92c17a39b84e70000d31e306a4fed6d6
salehadian <sahmedi@ucsd.edu>
2023-09-20 21:39:09 -0700

    Updated events page, still need to integrate g-cal and edit footer

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,14 +1,23 @@
 // Import Dependencies
 import '../styles/Events.css';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 function Events() {
   return (
     <div className="eventsContainer">
       <header className="eventsNavbar">
         <Navbar />
-
       </header>
+      
+      <div className="eventsTitle">
+        <h1>Upcoming Events</h1>
+      </div>
+    
+      <footer className="homeFooter">
+        <Footer />
+      </footer>
+
     </div>
   );
 }
