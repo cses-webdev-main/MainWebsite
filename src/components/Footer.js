005006da99e12a3df7ff9e98b3d288d773fc379c
Kevin Kim <kevink856@gmail.com>
2023-09-25 18:50:24 -0700

    Fix CSS for Header, Footer, Home

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 // Import Dependencies
 import "../styles/Footer.css";
 import { Link } from 'react-router-dom';
+import { IconContext } from 'react-icons';
 import { FaDiscord, FaInstagram, FaLinkedinIn} from 'react-icons/fa';
 
 /*
@@ -15,13 +16,13 @@ function Footer() {
 
           <div className="footerText">
             <Link to="https://www.linkedin.com/company/cses-webdev/mycompany/" className="footerLinks">
-					    <FaLinkedinIn />
+					    <FaLinkedinIn className="icon" />
 				    </Link>
             <Link to="https://www.instagram.com/cses_ucsd/" className="footerLinks">
-					    <FaInstagram />
+					    <FaInstagram className="icon" />
 				    </Link>
             <Link to="https://discord.gg/vd9aFu4V" className="footerLinks">
-					    <FaDiscord />
+					    <FaDiscord className="icon" />
 				    </Link>
           </div>
         </div>
